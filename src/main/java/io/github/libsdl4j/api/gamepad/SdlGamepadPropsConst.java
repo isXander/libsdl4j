15926def4282f49d373751de6f3719964f00904b
isXander <xandersmith2008@gmail.com>
2024-01-30 22:01:19 +0000

    Add gamepad/joystick prop consts

---
+++ src/main/java/io/github/libsdl4j/api/gamepad/SdlGamepadPropsConst.java
@@ -0,0 +1,14 @@
+package io.github.libsdl4j.api.gamepad;
+
+import static io.github.libsdl4j.api.joystick.SdlJoystickPropsConst.*;
+
+public final class SdlGamepadPropsConst {
+    private SdlGamepadPropsConst() {
+    }
+
+    public static final String SDL_PROP_GAMEPAD_CAP_MONO_LED_BOOLEAN = SDL_PROP_JOYSTICK_CAP_MONO_LED_BOOLEAN;
+    public static final String SDL_PROP_GAMEPAD_CAP_RGB_LED_BOOLEAN = SDL_PROP_JOYSTICK_CAP_RGB_LED_BOOLEAN;
+    public static final String SDL_PROP_GAMEPAD_CAP_PLAYER_LED_BOOLEAN = SDL_PROP_JOYSTICK_CAP_PLAYER_LED_BOOLEAN;
+    public static final String SDL_PROP_GAMEPAD_CAP_RUMBLE_BOOLEAN = SDL_PROP_JOYSTICK_CAP_RUMBLE_BOOLEAN;
+    public static final String SDL_PROP_GAMEPAD_CAP_TRIGGER_RUMBLE_BOOLEAN = SDL_PROP_JOYSTICK_CAP_TRIGGER_RUMBLE_BOOLEAN;
+}
---
+++ src/main/java/io/github/libsdl4j/api/joystick/SdlJoystickPropsConst.java
@@ -0,0 +1,12 @@
+package io.github.libsdl4j.api.joystick;
+
+public final class SdlJoystickPropsConst {
+    private SdlJoystickPropsConst() {
+    }
+
+    public static final String SDL_PROP_JOYSTICK_CAP_MONO_LED_BOOLEAN = "SDL.joystick.cap.mono_led";
+    public static final String SDL_PROP_JOYSTICK_CAP_RGB_LED_BOOLEAN = "SDL.joystick.cap.rgb_led";
+    public static final String SDL_PROP_JOYSTICK_CAP_PLAYER_LED_BOOLEAN = "SDL.joystick.cap.player_led";
+    public static final String SDL_PROP_JOYSTICK_CAP_RUMBLE_BOOLEAN = "SDL.joystick.cap.rumble";
+    public static final String SDL_PROP_JOYSTICK_CAP_TRIGGER_RUMBLE_BOOLEAN = "SDL.joystick.cap.trigger_rumble";
+}
